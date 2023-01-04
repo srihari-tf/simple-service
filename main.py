d443873fdc15bd8445033b69a0c4757613a9dada
srihari radhakrishna <100119331+srihari-tf@users.noreply.github.com>
2023-01-04 19:47:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import gradio as gr
 def greet(name):
     return "Hello " + name.capitalize() + "!"
 
-gr.Interface(fn=greet, inputs="text", outputs="text").launch(server_name='0.0.0.0', server_port=8080)
+gr.Interface(fn=greet, inputs="text", outputs="text").launch(server_name='0.0.0.0', server_port=8000)
